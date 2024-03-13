9b383234b8e133b8464857d842bb97568aba0d05
jrdp2706 <celc05lerdo@gmail.com>
2024-03-13 09:15:57 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+
+
+// Declarar un array de nombres 
+let nombres = ["Ana", "Carlos", "Elena" , "Adriana" , "Luis"];
+// Acceder y mostrar elementos del array 
+console.log("Primer nombre:", nombres[0]);
+console.log("Número de nombres en el array:", nombres.length);
+// Añadir un nuevo nombre al array 
+nombres.push("Daniel");
+// Mostrar el array actualizado 
+console.log("Nombres actualizados:", nombres);
+
+// 1beibol 2taekwondo 3basquetball 4box 5americano
+
+// Declarar un array de deportes 
+let deportes = ["beisball", "taekwondo", "basquetball" , "box" , "americano"];
+// Acceder y mostrar elementos del array 
+console.log("Primer deporte:", deportes[0]);
+console.log("Número de deportes en el arreglo:", deportes.length);
+// Añadir un nuevo deporte al array 
+deportes.push("bolyball");
+// Mostrar el array actualizado 
+console.log("deportes actualizados:", deportes);
+
+
+
+// Declarar un objeto representando a una persona 
+let persona = { nombre: "María", edad: 30, ciudad: "Madrid" }; 
+// Acceder y mostrar propiedades del objeto 
+console.log("Nombre:", persona.nombre);
+ console.log("Edad:", persona.edad); 
+ console.log("Ciudad:", persona.ciudad);
+
+
+
+ // Declarar las variables nota1, nota2 y nota3 y asignarles valores numéricos
+var nota1 = 8.5;
+var nota2 = 7.0;
+var nota3 = 9.2;
+
+// Calcular el promedio
+var promedio = (nota1 + nota2 + nota3) / 3;
+
+// Mostrar el resultado del promedio en la consola
+console.log('El promedio es: ' + promedio.toFixed(2)); // toFixed(2) limita los decimales a dos
+
+
+var lado1 = 98;
+var lado2 = 306;
+
+var forc = (lado1 * lado2) ;
+
+    console.log (forc)
+
+
+    var base1 = 45;
+    var altura2 = 76;
+
+    var forr = (base1 * altura2) ;
+
+    console.log (forr) ; 
+
+
+    var base1 = 
